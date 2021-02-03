2a101becd1bd40896e49eeff702488ebf9088100
shljshlj <34565607+shljshlj@users.noreply.github.com>
2021-02-03 22:00:10 +0100

    Fork from main branch of antonia_website project, with some changed structure

---
+++ config/webpack.common.js
@@ -0,0 +1,47 @@
+const paths = require('./paths');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  // Where webpack looks to start building the bundle
+  entry: [paths.src + '/index.js'],
+
+  // Determine how modules within project are treated
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.html$/,
+        loader: 'html-loader',
+      },
+      {
+        test: /\.(svg|png|jpe?g|gif)$/i,
+        use: {
+          loader: 'file-loader',
+          options: {
+            esModule: false,
+            name: '[name].[hash].[ext]',
+            outputPath: 'assets/images',
+          },
+        },
+      },
+    ],
+  },
+
+  // Customize the webpack build process
+  plugins: [
+    // Removes/cleans build folders and unused assets when rebuilding
+    new CleanWebpackPlugin(),
+
+    // Generates an HTML file from a template
+    new HtmlWebpackPlugin({
+      filename: 'index.html', // output file
+      template: paths.src + '/index.html', // template file
+      favicon: false,
+    }),
+  ],
+}
