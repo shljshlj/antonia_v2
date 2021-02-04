43c1cb92b36e654882ca78c3d5f4ec5c02f5dbc8
shljshlj <34565607+shljshlj@users.noreply.github.com>
2021-02-05 00:06:38 +0100

    Add working link for resume

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -19,7 +19,7 @@ module.exports = {
         loader: 'html-loader',
       },
       {
-        test: /\.(svg|png|jpe?g|gif|pdf)$/i,
+        test: /\.(svg|png|jpe?g|gif)$/i,
         use: {
           loader: 'file-loader',
           options: {
